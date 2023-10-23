315f41065637a65939146de962435818f88ef4bb
Raj Kasaudhan <rajksd9@gmail.com>
2023-10-23 19:40:32 +0530

    updated CRUD operation on city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,25 +1,54 @@
 const { City } = require("../models/index");
 
 class CityRepository {
+  // creating a new city
   async createCity({ name }) {
     try {
       const city = await City.create({ name });
       return city;
     } catch (error) {
       console.log(error);
+      throw { error };
     }
   }
-  async deleteCity({ cityId }) {
+
+  // deleting a city
+  async deleteCity(cityId) {
     try {
       await City.destroy({
         where: {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log(error);
       throw { error };
     }
   }
+
+  // get a particular city
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPK(cityId);
+      return city;
+    } catch (error) {
+      console.log(error);
+      throw { error };
+    }
+  }
+
+
+// update a city
+ async updateCity(cityId,{name}){
+  try{
+    const city = await City.update(data,{where:{id:cityId}})
+    return city;
+  }
+
+ }
+
 }
 
 module.exports = CityRepository;
